f02c104b184c58f8fc3b5a0aca049a0c3c7df414
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-12 16:59:18 +0600

    user serializer

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'email', 'username', 'password')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username')

c55693d52d04ef78e4b8a806cab14db53c3fbdb5
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-12 17:34:34 +0600

    views created

--- store/views.py
+++ store/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Review
+from .serializers import ProductListSerializer, ProductDetailSerializer, ReviewSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['sizes__name', 'colors__name']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListSerializer
+        return ProductDetailSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def perform_create(self, serializer):
+        product = Product.objects.get(pk=self.kwargs['product_pk'])
+        serializer.save(user=self.request.user, product=product)

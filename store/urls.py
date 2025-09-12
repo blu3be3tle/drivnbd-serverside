2e3411dc7ea3363ded361291b5e4a9193790900a
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-12 21:31:45 +0600

    added wishlist url

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,4 @@
-from .views import ProductViewSet, ReviewViewSet, OrderViewSet
+from .views import ProductViewSet, ReviewViewSet, OrderViewSet, WishlistAPIView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
@@ -15,4 +15,6 @@ products_router.register('reviews', ReviewViewSet, basename='product-reviews')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(products_router.urls)),
+    path('wishlist/', WishlistAPIView.as_view(),
+         name='wishlist'),
 ]

b5a7e596011c8908334cfef77b241c29bf5a7147
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-29 07:23:21 +0600

    orderItem model created

--- order/models.py
+++ order/models.py
@@ -48,3 +48,17 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return f"Order {self.id} by {self.user.first_name} - {self.status}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+

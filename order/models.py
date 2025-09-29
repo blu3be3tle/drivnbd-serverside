626ec4e49ea2aedb53383b1870f3575b5553f66d
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-29 07:19:38 +0600

    order model created

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 from store.models import Product
+from uuid import uuid4
 
 
 class Cart(models.Model):
@@ -19,5 +20,31 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.quantity} x {self.product.name}"
+
+
+class Order(models.Model):
+    NOT_PAID = 'Not Paid'
+    READY_TO_SHIP = 'Ready To Ship'
+    SHIPPED = 'Shipped'
+    DELIVERED = 'Delivered'
+    CANCELED = 'Canceled'
+    STATUS_CHOICES = [
+        (NOT_PAID, 'Not Paid'),
+        (READY_TO_SHIP, 'Ready To Ship'),
+        (SHIPPED, 'Shipped'),
+        (DELIVERED, 'Delivered'),
+        (CANCELED, 'Canceled')
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders")
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default=NOT_PAID)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Order {self.id} by {self.user.first_name} - {self.status}"

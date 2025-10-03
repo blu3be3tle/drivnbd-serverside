92b1c5ab2b66d85cd8689c60dd48317b815ebace
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-04 00:25:22 +0600

    created router paths

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path, include
+from store.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+
+router = DefaultRouter()
+router.register('products', ProductViewSet, basename='products')
+router.register('categories', CategoryViewSet)
+
+product_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+product_router.register('reviews', ReviewViewSet, basename='product-review')
+product_router.register('images', ProductImageViewSet,
+                        basename='product-images')
+
+
+# urlpatterns = router.urls
 
 urlpatterns = [
-    path('products/', include('store.product_urls')),
-    path('categories/', include('store.category_urls'))
+    path('', include(router.urls)),
+    path('', include(product_router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
--- drivnbd/urls.py
+++ drivnbd/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from .views import api_root_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("products/", include("store.urls")),
-    path("api/v1", include("api.urls")),
+    path('admin/', admin.site.urls),
+    path('', api_root_view),
+    path('api/v1/', include('api.urls'), name='api-root')
 ]
 
 
